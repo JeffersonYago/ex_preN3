c43785420ecea7d2bd3b7e5929f3da3c7bf2a782
JeffersonYago <jefferson.almeida3@fatec.sp.gov.br>
2023-12-03 06:19:39 -0300

    Update Principal.java

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -12,3 +12,9 @@ public class Principal {
 			
 	}
 	}
+/*Pegar o conteúdo do site: https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita 
+ * (Usar modo RAW ou Dados Brutos), esse site traz um arquivo com diferentes formas de se preparar 
+ * uma Marguerita. Gravar em um arquivo chamado marg.json na pasta C:\TEMP (\tmp se for Linux) e 
+ * fazer uma aplicação java que, na classe principal, no método Main, chame uma classe de controle que leia o 
+ * arquivo, e, para todas as receitas, apresente as instruções em italiano (strInstructionsIT)
+ */
